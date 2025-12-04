6f1051f833d54bd13f2bb805b0bb62507d298bab
shanu106 <shahnawajnilger244@gmail.com>
2025-12-04 14:12:35 +0530

    pincode log

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(cors({
 // };
 
 // app.use(cors(corsOptions));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Basic routes
