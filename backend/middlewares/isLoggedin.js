5e96244609f4a21c857e3a14dd1ef52f29b97ddd
shanu106 <shahnawajnilger244@gmail.com>
2025-12-04 13:54:21 +0530

    pincode log

--- backend/middlewares/isLoggedin.js
+++ backend/middlewares/isLoggedin.js
@@ -5,7 +5,7 @@ const userModel = require('../models/User.model');
 module.exports = async function (req, res, next) {
 
    
-   console.log(req)
+   console.log("is logged in  req ",req)
     const token = req.headers.Authorization? req.headers.Authorization.split(' ')[1]: req.cookies.token;
  if (!token) {
    console.log("no token");
